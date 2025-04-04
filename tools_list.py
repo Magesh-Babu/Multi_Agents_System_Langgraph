49a70e764e090ac1189f8a0c87407d109c6b09a4
magesh-babu <maheshkannan78@gmail.com>
2025-04-04 12:21:56 +0200

    correction_and_update

--- tools_list.py
+++ tools_list.py
@@ -17,6 +17,7 @@ class RagToolSchema(BaseModel):
     question: str
     ticker: str
 
+# Pydantic schema for the financial ratio tools
 class RatioToolSchema(BaseModel):
     """
     Schema for finance tool input.
@@ -25,8 +26,9 @@ class RatioToolSchema(BaseModel):
     which includes financial ticker symbol.
     """
 
-    query: str
+    ticker: str
 
+# Pydantic schema for the region_scb tool
 class RegionToolSchema(BaseModel):
     region: Literal['Sweden', 'Greater Stockholm',
     'Greater Gothenburg',
@@ -39,7 +41,6 @@ class RegionToolSchema(BaseModel):
     'Northern Central Sweden',
     'Central Norrland',
     'Upper Norrland']
-
 
 
 # Tool to retrieve semantically similar documents based on a user question
@@ -92,11 +93,19 @@ def retriever_tool(question: str, ticker: str) -> str:
 
 
 @tool(args_schema=RatioToolSchema)
-def income_statement_tool(query):
-    """Tool to retrieve the income statement of past 5 years from the company stock ticker"""
+def income_statement_tool(ticker):
+    """
+    Tool to retrieve the income statement of past 5 years from the company's stock ticker.
+
+    Args:
+        ticker (str): The stock ticker symbol for retrieving relevant news.
+
+    Returns:
+        dataframe: The income statement data.    
+    """
     try:
         print("\nUSING INCOME_STMT_TOOL\n")
-        fetcher = FinancialDataFetcher(query)
+        fetcher = FinancialDataFetcher(ticker)
         return fetcher.get_income_statement()
     except Exception as e:
         print(f"Error in income_statement_tool: {e}")
@@ -104,11 +113,11 @@ def income_statement_tool(query):
 
 
 @tool(args_schema=RatioToolSchema)
-def balance_sheet_tool(query):
+def balance_sheet_tool(ticker):
     """Tool to retrieve the balance sheet of past 5 years from the company stock ticker"""
     try:
         print("\nUSING BALANCE_SHEET TOOL\n")
-        fetcher = FinancialDataFetcher(query)
+        fetcher = FinancialDataFetcher(ticker)
         return fetcher.get_balance_sheet()
     except Exception as e:
         print(f"Error in balance_sheet_tool: {e}")
@@ -116,11 +125,11 @@ def balance_sheet_tool(query):
 
 
 @tool(args_schema=RatioToolSchema)
-def cashflow_tool(query):
+def cashflow_tool(ticker):
     """Tool to retrieve the cash flow details of past 5 years from the company stock ticker"""
     try:
         print("\nUSING CASHFLOW TOOL\n")
-        fetcher = FinancialDataFetcher(query)
+        fetcher = FinancialDataFetcher(ticker)
         return fetcher.get_cashflow()
     except Exception as e:
         print(f"Error in cashflow_tool: {e}")
@@ -128,11 +137,11 @@ def cashflow_tool(query):
 
 
 @tool(args_schema=RatioToolSchema)
-def finance_ratio_tool(query):
+def finance_ratio_tool(ticker):
     """Tool to retrieve the financial ratios/details from the company stock ticker"""
     try:
         print("\nUSING RATIOS TOOL\n")
-        fetcher = FinancialDataFetcher(query)
+        fetcher = FinancialDataFetcher(ticker)
         return fetcher.get_basic_financials()
     except Exception as e:
         print(f"Error in basic_finance_tool: {e}")
